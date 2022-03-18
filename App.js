c30fef29ddc0cf6bd4d49e415a0a94415e05361f
Kevin Tan <tankevin@stanford.edu>
2022-03-17 23:45:30 -0700

    🚨 Fix misc. linting errors

--- App.js
+++ App.js
@@ -12,14 +12,14 @@ import { Icons, Themes } from './assets/Themes'
 import { Platform } from 'react-native-web'
 
 export default function App () {
-  let [fontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Sydney: require('./assets/Fonts/Sydney-Serial-Regular.ttf'),
     'Sydney-Bold': require('./assets/Fonts/Sydney-Serial-Bold.ttf')
   })
   if (!fontsLoaded) return <AppLoading />
   /* ^Don't mind/edit the code above, it's there to load the font for you! */
   StatusBar.setBarStyle(Themes.light.statusBar)
-  /* ^Don't mind/edit this one either unless you decide to do the dark theme one, in that case, you will have to change it accordingly*/
+  /* ^Don't mind/edit this one either unless you decide to do the dark theme one, in that case, you will have to change it accordingly */
 
   /* insert your code here */
 
